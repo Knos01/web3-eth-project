d41b5914313763df91a2914f1d93b3240a3440fa
Christian Kobril <christiankobril@gmail.com>
2022-03-11 10:42:57 +0100

    create counter contract and estimate gas

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,19 @@
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
+import "hardhat-gas-reporter";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
     solidity: "0.8.10",
+    networks: {
+        hardhat: {
+            chainId: 1337
+        },
+        // rinkeby: {
+        //     url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+        //     accounts: [`${process.env.RINKEBY_KEY}`]
+        // }
+    }
 };
---
+++ scripts/deploy-counter.ts
@@ -0,0 +1,20 @@
+import "@nomiclabs/hardhat-ethers";
+import { Contract } from 'ethers';
+import { ethers } from 'hardhat';
+
+async function deploy() {
+    const Counter = await ethers.getContractFactory("Counter");
+    const counter = await Counter.deploy();
+    await counter.deployed();
+
+    console.log(counter.address)
+    return counter;
+}
+
+// @ts-ignore
+async function count(counter) {
+    await counter.count();
+    console.log("Counter: ", await counter.getCounter());
+}
+
+deploy().then(count)
